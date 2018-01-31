bdbe0ca094a8b786d6315bf0a8cbf0bf71a1a24b
Sabahat Hussain <31700640+iamsabahat@users.noreply.github.com>
2018-01-31 13:38:12 +0530

    Update main.py

--- WebCrawler/main.py
+++ WebCrawler/main.py
@@ -12,3 +12,17 @@ CRAWLED_FILE = PROJECT_NAME + '/whatsDone.txt'
 NUMBER_OF_THREADS = 8
 queue = Queue
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+def crawl():
+    queued_links = fileSetter(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print(str(len(queued_links)) + ' Links on list.')
+        createJobs()
+
+
+def createJobs():
+    for links in fileSetter(QUEUE_FILE):
+        queue.put(links)
+        queue.join()
+        crawl()
