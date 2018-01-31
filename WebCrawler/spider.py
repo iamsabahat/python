9f3af91d9271c52d1b7d30ea2eba4725a9d3839e
Sabahat Hussain <31700640+iamsabahat@users.noreply.github.com>
2018-01-31 13:38:59 +0530

    Update spider.py

--- WebCrawler/spider.py
+++ WebCrawler/spider.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from finder import Finder
 from demo import *
-
+from domain import *
 
 class Spider:
 
@@ -23,9 +23,51 @@ class Spider:
         self.boot()
         self.crawlPage('First Spider', Spider.baseURL)
 
-
+    @staticmethod
     def boot(self):
         createDirectory(Spider.projectName)
         createDataFiles(Spider.projectName, Spider.baseURL)
         Spider.queue = fileSetter(Spider.onQueue)
         Spider.crawled = fileSetter(Spider.itsDone)
+
+
+    @staticmethod
+    def crawlpage(thread_name, pageURL):
+        if pageURL not in Spider.crawled:
+            print(thread_name + 'Spider is now crawling => ' + pageURL)
+            print('Queue : ' + str(len(Spider.queue)) + ' | Crawled : ' + str(len(Spider.crawled)))
+            Spider.addLinksToList(Spider.gatherLinks(pageURL))
+            Spider.queue.remove(pageURL)
+            Spider.crawled.add(pageURL)
+            Spider.update_files()
+
+
+    @staticmethod
+    def gatherLinks(pageURL):
+        html_string = ''
+        try:
+            response = urlopen(pageURL)
+            if 'text/html' in response.getheader('Content-Type'):
+                html_bytes = response.read()
+                html_string = html_bytes.decode("utf-8")
+            finderObject = Finder(Spider.baseURL, pageURL)
+            finderObject.feed(html_string)
+        except Exception as e:
+            print(str(e))
+            return set()
+        return finderObject.pageLink()
+
+
+    @staticmethod
+    def addLinksToList(links):
+        for urls in links:
+            if(urls in Spider.queue) or (urls in Spider.crawled):
+                continue
+            if Spider.domain != get_domain_name(urls):
+                continue
+            Spider.queue.add(urls)
+
+    @staticmethod
+    def update_files():
+        setFile(Spider.queue, Spider.onQueue)
+        setFile(Spider.crawled, Spider.itsDone)
