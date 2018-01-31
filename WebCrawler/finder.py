4577a6179c47e9549bc25e4fbae7fe823f9a7a6a
Sabahat Hussain <31700640+iamsabahat@users.noreply.github.com>
2018-01-31 12:59:26 +0530

    Update finder.py

--- WebCrawler/finder.py
+++ WebCrawler/finder.py
@@ -1,1 +1,24 @@
+from html.parser import HTMLParser
+from urllib import parse
 
+
+class Finder(HTMLParser):
+
+    def __init__(self, baseURL, pageURL):
+        super.__init__()
+        self.baseURL = baseURL
+        self.pageURL = pageURL
+        self.links = set()
+
+    def errorFinder(self, message):
+        pass
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    urlLinker = parse.urljoin(self.baseURL, value)
+                    self.links.add(urlLinker)
+
+    def pageLink(self):
+        return self.links
