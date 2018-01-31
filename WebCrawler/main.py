bfada69400b33f8bd75b58c99843b71917fcc593
Sabahat Hussain <31700640+iamsabahat@users.noreply.github.com>
2018-01-31 13:31:23 +0530

    Update main.py

--- WebCrawler/main.py
+++ WebCrawler/main.py
@@ -1,1 +1,14 @@
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from demo import *
 
+PROJECT_NAME = 'thesite'
+HOMEPAGE = 'http://wishfie.com/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/onList.txt'
+CRAWLED_FILE = PROJECT_NAME + '/whatsDone.txt'
+NUMBER_OF_THREADS = 8
+queue = Queue
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
