d467e7a0dc4cbe3d47d49c19593ff91822d6fef8
Sabahat Hussain <31700640+iamsabahat@users.noreply.github.com>
2018-01-31 13:30:08 +0530

    Update domain.py

--- WebCrawler/domain.py
+++ WebCrawler/domain.py
@@ -1,1 +1,14 @@
+from urllib.parse import urlparse
 
+def get_domain_name(urls):
+    try:
+        results = get_sub_domain_name(urls).split('.')
+        return results[-2] + '.' + results[-1]
+    except:
+        return ''
+
+def get_sub_domain_name(urls):
+    try:
+        return urlparse(urls).netloc
+    except:
+        return ''
